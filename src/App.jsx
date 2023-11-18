dde06b5d357fa086151d391fd4a2c9594c95001c
Nico Diaz <nicodiazan@gmail.com>
2023-11-18 16:56:05 +0100

    Add search function and a way to detect status 200

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import "./App.css";
 import api from "../api";
 
 function App() {
+	const [search, setSearch] = useState("cats");
 	const [img, setImg] = useState(0);
-	useEffect(() => {
+    const [error, setError] = useState(false);
+	function fetchNew() {
 		fetch(
 			"https://api.giphy.com/v1/gifs/translate?api_key=" +
 				api.key +
-				"&s=cats",
+				"&s=" +
+				search,
 			{ mode: "cors" }
 		)
 			.then(function (response) {
+                if(response.status === 200) setError(true)
 				return response.json();
 			})
 			.then(function (response) {
@@ -21,10 +25,30 @@ function App() {
 			.catch(function (error) {
 				console.log(error);
 			});
-	}, []);
-	return <>
-    <img src={img} alt="cat idk" />
-    </>;
+	}
+	useEffect(() => {
+		fetchNew();
+	}, [search]);
+	return (
+		<>
+			<input
+				type="text"
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+				onKeyUp={(e) => {
+					if (e.key === "Enter") {
+						fetchNew();
+					}
+				}}
+			/>
+            {
+                error?
+                <img src={img} alt={search + " image"} />
+                :
+                <p>Error: {search} not found</p>
+            }
+		</>
+	);
 }
 
 export default App;
